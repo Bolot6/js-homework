ad8a6a3530b85ebebf535e82c76d00758be5a064
bolot6 <kurmanaliev78@gmail.com>
2023-10-25 14:35:05 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+// boolean = true/false
+// string = ''/""
+// number = 1
+// undefined = 
+// null = 
+// symbol = 
+// bigInt =big numbers
+
+// let a = 4
+// let b = 55
+// console.log(a+b)
+
+// let a = 4
+// let b = 55
+// console.log(a*b)
+
+// let a = 4
+// let b = 55
+// console.log(a/b)
+
+// let a = 4
+// let b = 55
+// console.log(a%b)
+
+// let a = 'hello '
+// let b = 'Bolot'
+// console.log(a+b)
+
+// let a = 5
+// let b = 'Hasbik'
+// console.log(a-b)
+
+let a = 5
+let b = '5'
+console.log(Number(b) + a)
+
